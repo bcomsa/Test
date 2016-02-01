65864831660be006a0ab9f4aa96b0776760094ea
Tam Vu Quang <vuquangtam1994@gmail.com>
2016-02-01 16:41:42 +0700

    Add Kivy Blueprint Examples

---
+++ Kivy Test/pagelayout.py
@@ -0,0 +1,8 @@
+from kivy.app import App
+
+class PageLayoutApp(App):
+    def build(self):
+        return
+
+if __name__ == "__main__":
+    PageLayoutApp().run()
---
+++ Kivy Test/stream.py
@@ -0,0 +1,8 @@
+from kivy.app import App
+
+class StreamApp(App):
+    def build(self):
+        return
+
+if __name__ == "__main__":
+    StreamApp().run()
